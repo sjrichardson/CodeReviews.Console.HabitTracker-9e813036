07bc8d761e4c0d087ee48bb93d309d855b0f69ea
Sam Richardson <samrichardson812@gmail.com>
2026-04-14 16:46:57 -0500

    Move Dialogue code to DialogueService

--- habit-tracker/src/Dialogue/DialogueService.cs
+++ habit-tracker/src/Dialogue/DialogueService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,35 @@ namespace habit_tracker.src.Dialogue
         {
             Console.WriteLine($"\n\n{message} \n\n");
         }
+        
+        public string GetDateInput()
+        {
+            Console.WriteLine("Please insert the date: (Format: dd-mm-yy). Type 0 to return to Main Menu.");
+
+            string? dateInput = Console.ReadLine();
+            while(!DateTime.TryParseExact(dateInput, "dd-MM-yy",new CultureInfo("en-US"), DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("Invalid date. (Format: dd-mm-yy) Try again or press 0 to return to Main Menu.");
+                dateInput = Console.ReadLine();
+            }
+
+            return dateInput;
+        }
+        
+        public int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+
+            string? numberInput = Console.ReadLine();
+
+            while(!Int32.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
+            {
+                Console.WriteLine("Invalid number. Try again or press 0 to return to Main Menu");
+                numberInput = Console.ReadLine();
+            }
+            
+            return Convert.ToInt32(numberInput);
+        }
     }
+
 }
